2e8baea1d6ad4e33384e5b2b654488678bcc3dda
paulshu <paulshu18@gmail.com>
2017-06-17 21:06:05 +0800

    set time_zone

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :events
-
+  resource :user
+  #这里的路由设计使用单数 resource :user，跟 resources :users 相比，单数的路由少了 index action
   namespace :admin do
     root "events#index"
     resources :events
