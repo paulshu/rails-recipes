db20d8fc9a961843f2be10d8aece8c588da4ef54
paulshu <paulshu18@gmail.com>
2017-06-24 10:21:51 +0800

    nested_form_fields gem

--- Gemfile
+++ Gemfile
@@ -35,7 +35,7 @@ gem 'jbuilder', '~> 2.5'
 gem "rails-i18n"
 gem "devise-i18n"
 gem "select2-rails"
-
+gem "nested_form_fields"
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
--- app/models/event.rb
+++ app/models/event.rb
@@ -5,6 +5,8 @@ class Event < ApplicationRecord
  validates_format_of :friendly_id, :with => /\A[a-z0-9\-]+\z/
  before_validation :generate_friendly_id, :on => :create
  belongs_to :category, :optional => true
+ has_many :tickets, :dependent => :destroy, :inverse_of => :event
+ accepts_nested_attributes_for :tickets, :allow_destroy => true, :reject_if => :all_blank
 
  def to_param
   # "#{self.id}-#{self.name}"
