e2abad9546a6ff37c03c4f3b7d9730159eaab1ca
paulshu <paulshu18@gmail.com>
2017-07-10 23:01:16 +0800

    ckeditor &bulk_delete & bulk_update

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,18 @@ Rails.application.routes.draw do
   #这里的路由设计使用单数 resource :user，跟 resources :users 相比，单数的路由少了 index action
   namespace :admin do
     root "events#index"
-    resources :events
+    resources :events do
+      resources :tickets, :controller => "event_tickets"
+      collection do
+        post :bulk_update
+      end
+    end
     resources :users do
       resource :profile, :controller => "user_profiles"
       #因为默认的 controller 的命名是 profiles，这里我们偏好自定义命名改为 user_prorfiles
     end
   end
+
 
   root "events#index"
   get "/faq" => "pages#faq"
