872c91a96628919d19f1abe13a72224cb8dd64cf
paulshu <paulshu18@gmail.com>
2017-07-13 21:22:19 +0800

    imcomplement Multi-step form function

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,4 +1,7 @@
 class Registration < ApplicationRecord
+  attr_accessor :current_step
+  validates_presence_of :name, :email, :cellphone, :if => :should_validate_basic_data?
+  validates_presence_of :name, :email, :cellphone, :bio, :if => :should_validate_all_data?
 
   STATUS = ["pending", "confirmed"]
   validates_inclusion_of :status, :in => STATUS
@@ -18,5 +21,13 @@ class Registration < ApplicationRecord
 
   def generate_uuid
     self.uuid = SecureRandom.uuid
+  end
+
+  def should_validate_basic_data?
+    current_step == 2 #只有做到第二步需要验证
+  end
+
+  def should_validate_all_data?
+    current_step == 3 || status == "confirmed" # 做到第三步，或最后状态是 confirmed 时需要验证
   end
 end
